b44b7dea8a7676cac45de57eeb288cd6743dc084
siljag23 <siljag23@ru.is>
2026-02-12 13:39:47 +0000

    Update Opna_excel.py

--- Opna_excel.py
+++ Opna_excel.py
@@ -3,4 +3,9 @@ import pandas as pd
 
 file = pd.read_excel("Input.xlsx")
 
-print(file)
+file = file.dropna(how="all")
+file = file.loc[:,~file.columns.str.contains("^Unnames")]
+
+result = file.groupby(file.columns[0])[file.columns[1]].apply(list).to_dict()
+
+print(result)
