a37ffb92301c2c2339f5546cae7b2dff75a6236e
KatrinSigurdard <katrin23@ru.is>
2026-02-12 14:36:02 +0000

    Update Opna_excel.py

--- Opna_excel.py
+++ Opna_excel.py
@@ -1,4 +1,3 @@
-
 def open_excel(file_name, sheet_1_name, sheet_2_name):
 
     import pandas as pd
@@ -7,7 +6,7 @@ def open_excel(file_name, sheet_1_name, sheet_2_name):
     employees  = pd.read_excel(file_name, sheet_name=sheet_2_name)
 
 
-    # HHreinsum skjalið
+    # Hreinsum skjalið
     events = events.dropna(how="all")
     employees = employees.dropna(how="all")
     events = events.loc[:, ~events.columns.str.contains("^Unnamed")]
