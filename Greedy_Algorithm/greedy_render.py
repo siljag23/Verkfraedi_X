1289d1460ee18ed45634b1e4db6443e903fa6788
KatrinSigurdard <katrin23@ru.is>
2026-04-29 20:58:23 +0000

    laga greedy_render

--- Greedy_Algorithm/greedy_render.py
+++ Greedy_Algorithm/greedy_render.py
@@ -1,6 +1,6 @@
 
-from Greedy_Algorithm.open_excel import open_excel, open_previous_scores, open_previous_stats, merge_scores_into_employees, merge_previous_stats_into_employees
-from Greedy_Algorithm.pick_employees import assign_all_events
+from open_excel import open_excel, open_previous_scores, open_previous_stats, merge_scores_into_employees, merge_previous_stats_into_employees
+from pick_employees import assign_all_events
 from Print_Results_Greedy import Print_Results_Greedy
 from Export_Json_Greedy import Export_Json
 from export_schedule_to_excel_greedy import export_schedule_to_excel
