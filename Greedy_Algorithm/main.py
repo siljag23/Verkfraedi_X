413deca0b1e39919058991603b921128951394f0
KatrinSigurdard <katrin23@ru.is>
2026-04-29 21:45:04 +0000

    laga main

--- Greedy_Algorithm/main.py
+++ Greedy_Algorithm/main.py
@@ -5,28 +5,17 @@ from greedy_render import run_greedy
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Vaktaplan app is running"}
-
-
 @app.post("/run")
 async def run(file: UploadFile = File(...)):
 
-    # tryggja að Data mappa sé til
     os.makedirs("Data", exist_ok=True)
 
-    # fá nafn á file (t.d. 03_26.xlsx)
-    filename = file.filename
-    month = filename.replace(".xlsx", "")
+    input_path = os.path.join("Data", file.filename)
 
-    # vista í Data möppu
-    input_path = os.path.join("Data", filename)
-
+    # vista file
     with open(input_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    # keyra greedy
     result = run_greedy(input_path)
 
     return result
