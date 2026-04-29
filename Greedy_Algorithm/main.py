2f89b590113b92476b3db49907d19cc95f5a665f
KatrinSigurdard <katrin23@ru.is>
2026-04-29 21:52:17 +0000

    laga main

--- Greedy_Algorithm/main.py
+++ Greedy_Algorithm/main.py
@@ -10,12 +10,12 @@ async def run(file: UploadFile = File(...)):
 
     os.makedirs("Data", exist_ok=True)
 
-    input_path = os.path.join("Data", file.filename)
+    filename = file.filename
+    input_path = os.path.join("Data", filename)
 
-    # vista file
     with open(input_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    result = run_greedy(input_path)
+    result = run_greedy(filename)
 
     return result
