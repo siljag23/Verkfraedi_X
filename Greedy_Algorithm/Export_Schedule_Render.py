30c747e02ce31dd8010df39d9e39333009200b4d
KatrinSigurdard <katrin23@ru.is>
2026-05-10 16:58:28 +0000

    laga plot i excel

--- Greedy_Algorithm/Export_Schedule_Render.py
+++ Greedy_Algorithm/Export_Schedule_Render.py
@@ -400,6 +400,8 @@ def Export_Schedule_Render(
         chart1.x_axis.tickLblPos = "low"
         chart1.x_axis.delete = False
         chart1.x_axis.textRotation = 45
+        chart1.width = 30
+        chart1.height = 12
 
         for s in chart1.series:
             s.graphicalProperties.solidFill = "FF6E1B"
@@ -423,7 +425,8 @@ def Export_Schedule_Render(
         chart2.x_axis.tickLblPos = "low"
         chart2.x_axis.delete = False
         chart2.x_axis.textRotation = 45
-
+        chart2.width = 30
+        chart2.height = 12
         for s in chart2.series:
             s.graphicalProperties.solidFill = "FF6E1B"
 
