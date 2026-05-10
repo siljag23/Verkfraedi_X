42cb015aacf74745b7d4f30939655979a1e58844
KatrinSigurdard <katrin23@ru.is>
2026-05-10 17:10:11 +0000

    .

--- Greedy_Algorithm/Export_Schedule_Render.py
+++ Greedy_Algorithm/Export_Schedule_Render.py
@@ -402,7 +402,7 @@ def Export_Schedule_Render(
         chart1.title.overlay = False
         chart1.x_axis.textRotation = 45
         chart1.width = 30
-        chart1.height = 6
+        chart1.height = 8
 
         for s in chart1.series:
             s.graphicalProperties.solidFill = "FF6E1B"
@@ -425,10 +425,10 @@ def Export_Schedule_Render(
 
         chart2.x_axis.tickLblPos = "low"
         chart2.x_axis.delete = False
-        chart1.title.overlay = False
+        chart2.title.overlay = False
         chart2.x_axis.textRotation = 45
         chart2.width = 30
-        chart2.height = 6
+        chart2.height = 8
         for s in chart2.series:
             s.graphicalProperties.solidFill = "FF6E1B"
 
